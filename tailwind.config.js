f955d30471b085b80a80613ad7648505c3bd7e5e
bulentdenizz <bulent.dnz.99@gmail.com>
2026-02-27 14:42:24 +0300

    update

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/renderer/**/*.html",
+    "./src/renderer/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
